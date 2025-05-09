fed1df89bd40ac6a9fc4ef89cd1c7b1f6cffe684
Muhammad Zaki Jauhari <mzakijauhari@gmail.com>
2025-05-09 19:59:18 +0700

    konfigurasi github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/32-pbk-uts-233510607/', 
   plugins: [vue()],
 })
+
